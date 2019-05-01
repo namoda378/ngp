f2162b5ef093468d64e38cdbc200051326fd7af5
Namo Lee <namoda378@gmail.com>
2019-05-01 21:24:58 +0900

    config plugin source-filesystem transformer remark

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,12 +6,34 @@
 
 module.exports = {
   siteName: 'Gridsome',
+  transformers: {
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      anchorClassName: 'icon icon-link',
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
   plugins: [
     {
       use: 'gridsome-plugin-pug',
       options: {
           // pug: { /* Options for `pug-plain-loader` */ },
           // pugLoader: { /* Options for `pug-loader` */ }
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'blog/**/*.md',
+        typeName: 'Post',
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
       }
     }
   ],
