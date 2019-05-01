1b03237650932dd8f0ff2450e98b9de7b5961057
Namo Lee <namoda378@gmail.com>
2019-05-01 23:08:25 +0900

    first template with page-query, page is automatically placed where the source data file (md,yaml ... ) is

---
+++ src/templates/Markdown.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+    div
+        h1 {{ this.$page.markdown.title }}
+        h1 {{ this.$page.markdown.propA }}
+</template>
+
+<page-query>
+    query Markdown ($path:String!) {
+        markdown : markdown(path:$path){
+            title
+            propA
+        }
+    }
+</page-query>
+
+<script>
+export default {
+    metaInfo(){
+        return {
+            title: this.$page.title,
+        }
+    }
+}
+</script>
