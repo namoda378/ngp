48504638bbe57f20fb5c2c6cc402bfd40c3b3c52
Namo Lee <namoda378@gmail.com>
2019-05-02 16:11:15 +0900

    start implementing recursive content rendering

---
+++ src/templates/Yaml.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+
+    div
+        h1 {{ this.$page.markdown.title }}
+        h1 {{ this.$page.markdown.propA }}
+
+
+</template>
+
+<page-query>
+    query Yaml($path:String!) {
+        yaml : yaml(path:$path){
+            path
+            data
+        }
+    }
+</page-query>
+
+<script>
+export default {
+    data(){
+        return {
+            contentObject : JSON.parse(this.$page.data),
+        }
+    },
+    metaInfo(){
+        return {
+            title: this.$page.path,
+        }
+    }
+}
+</script>
