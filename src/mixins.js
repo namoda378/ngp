731138c694c018bdd0ff0d43de93e6a62e5407d8
Namo Lee <namoda378@gmail.com>
2019-05-20 23:52:29 +0900

    orientation change detection

--- src/mixins.js
+++ src/mixins.js
@@ -2,13 +2,12 @@ export const windowResizeListener = {
     mounted() {
       this.$nextTick(function() {
         window.addEventListener('resize', this.onWindowResize);
+        window.addEventListener('onorientationchange', this.onWindowResize);
       })
-    },
-  
-    methods: {
     },
     beforeDestroy() {
       window.removeEventListener('resize', this.onWindowResize);
+      window.removeEventListener('onorientationchange', this.onWindowResize);
     }
   };
 
